d44185645cb6b1f57b1d532dad55905088103c82
codeamac09 <codeamac09@gmail.com>
2016-04-05 20:37:03 -0500

    Completed

--- torteria.rb
+++ torteria.rb
@@ -4,41 +4,45 @@ class Torta
   attr_reader :tam #tamaño
   attr_reader :tipo_torta
   attr_reader :tiempo_coccion
-  attr_reader :coccion
+  attr_reader :estatus
 
-  def initialize(tipo_torta, tam)
+  def initialize(tipo_torta, tam, tiempo_coccion)
     @tam = tam
     @tipo_torta = tipo_torta
-    @tiempo_coccion = 0
-    @coccion = ""
+    @tiempo_coccion = tiempo_coccion
+    @status = ""
+
   end#mtd initialize
 
   def to_s
     "soy #{self} #{tipo_torta} #{@tam}"
   end#mtd to_s
 
-  def tiempo_coccion
-    case @tipo_torta
-      when "rusa"             then @tiempo_coccion = 8
-      when "argentina"        then @tiempo_coccion = 7
-      when "hawaiana"         then @tiempo_coccion = 4
-      when "cubana"           then @tiempo_coccion = 3
-      when "milanesa"         then @tiempo_coccion = 5
-      else
-        "Esa torta no está en el menú"
-    end
-  end#mtd tiempo_coccion
+  # def tiempo_coccion
+  #   case @tipo_torta
+  #     when "rusa"             then @tiempo_coccion = 8
+  #     when "argentina"        then @tiempo_coccion = 7
+  #     when "hawaiana"         then @tiempo_coccion = 4
+  #     when "cubana"           then @tiempo_coccion = 3
+  #     when "milanesa"         then @tiempo_coccion = 5
+  #     else
+  #       "Esa torta no está en el menú"
+  #   end
+  # end#mtd tiempo_coccion
 
   def coccion
-    tiempo_coccion
     if $tiempo_horno < @tiempo_coccion - 1
-      @coccion = "Torta #{tipo_torta} cruda"
+       "Torta #{tipo_torta} cruda"
+       @estatus = "cruda"
     elsif $tiempo_horno > @tiempo_coccion
-      @coccion = "Torta #{tipo_torta} se quemó"
+      "Torta #{tipo_torta} se quemó"
+      @estatus = "quemada"
     elsif $tiempo_horno == @tiempo_coccion - 1
-      @coccion = "Torta #{tipo_torta} casi lista"
+      "Torta #{tipo_torta} casi lista"
+      @estatus = "casi lista"
     else
-      @coccion = "Torta #{tipo_torta} lista!"
+      "Torta #{tipo_torta} lista!"
+      @estatus = "lista"
     end
   end#mtd torta_coccion
       
@@ -58,7 +62,7 @@ class Horno
   end
 
   def fill?
-    if @capacidad < 1
+    if @capacidad <= 3
       false
     else
       true
@@ -72,7 +76,11 @@ class Charola
   attr_reader :capacidad
 
   def initialize
-    @capacidad = 0.0
+    @capacidad = 0
+  end
+
+  def clean
+    @capacidad = 0
   end
 
   def fill(tam)
@@ -99,35 +107,62 @@ end#class charola
 
 horno = Horno.new
 charola = Charola.new
-quemadas = []
-listas = []
-tortas = []
-menu = %w( Rusa Argentina Hawaiana Cubana Milanesa)
-lineWidth = 60
-puts "Menú del día".center lineWidth
+quemadas = 0
+listas = 0
+crudas = 0
+casi_listas = 0
+tortas_aux = []
 
-until charola.fill?
-    puts "\t*Rusa  *Argentina  *Hawaiana  *Cubana  *Milanesa"
+op = "Y"
+until op == "N"
+    puts "\tIngresa de que quieres hacer tortas"
     tipo_torta = gets.chomp.downcase
-
-    puts "  Selecciona un tamaño"
-    puts "\t Mitad / Normal / Grande "
+    puts "\tElige su tamaño"
+    puts "\tMitad  (12 por charola)" 
+    puts "\tNormal (6 por charola)"
+    puts "\tGrande (4 por charola)"
     t = gets.chomp.downcase
+    puts "\tElige el tiempo de cocción"
+    tiempo_coccion = gets.chomp.to_i
 
-    tortas << Torta.new(tipo_torta, t)
-    charola.fill(t)
+    puts "\t¿Cuántas de éstas tortas deseas?"
+    cant = gets.chomp.to_i
+    for i in 1..cant
+      tortas_aux << Torta.new(tipo_torta, t, tiempo_coccion)
+    end
+    puts "Deseas ingresar más tortas? Y/N"
+    op = gets.chomp.upcase
 end
 
 until horno.fill?
-  $tiempo_horno = rand(9)
-  horno.fill
+    tortas_aux.each do |torta|
+        $tiempo_horno = rand(9)
+        until charola.fill?
+            charola.fill(torta.tam)
+            torta.coccion
+        end    
+        charola.clean
+    end   
+    horno.fill
 end
 
-tortas.each do |torta|
-  p torta.coccion
+tortas_aux.each do |torta|
+    if torta.estatus == "cruda"
+        crudas += 1
+    elsif torta.estatus == "quemada"
+        quemadas += 1
+    elsif torta.estatus == "casi lista"
+        casi_listas += 1
+    elsif torta.estatus == "lista"
+        listas += 0
+    end
 end
 
-
+puts "\tObtuviste #{listas} tortas listas!"
+puts "\tObtuviste #{casi_listas} tortas casi listas"
+puts "\tObtuviste #{quemadas} tortas quemadas! Ups!!"
+puts "\tObtuviste #{crudas} tortas crudas! Muy pronto!"
+puts "\tHiciste #{tortas_aux.count} tortas!"
 
 
 
